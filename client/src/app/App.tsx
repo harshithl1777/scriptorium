348794ffa03d30145307beba8b86a06e5257632d
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2024-11-29 14:36:52 -0500

    feat: setup basic search table

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from '@/components/ui/toaster';
 import LoginPage from '@/pages/LoginPage';
 import SignupPage from '@/pages/SignupPage';
+import Library from '@/pages/Library';
 import axios from 'axios';
 import { UserProvider } from '@/lib/UserProvider';
 import { SessionProvider } from '@/lib/SessionProvider';
@@ -40,9 +41,9 @@ function App() {
                             <Route
                                 path='/app/library'
                                 element={
-                                    <Protected>
-                                        <div>Library</div>
-                                    </Protected>
+                                    // <Protected>
+                                        <Library />
+                                    // </Protected>
                                 }
                             />
                         </Routes>
---
+++ client/src/pages/Library.tsx
@@ -0,0 +1,11 @@
+import { DataTableDemo } from "@/containers/SearchTable";
+
+function Library() {
+  return (
+    <div>
+      <DataTableDemo />
+    </div>
+  );
+}
+
+export default Library;
