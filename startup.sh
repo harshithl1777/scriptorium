57f2dcc98704aaed7b9a01c083e2e1a599cdbedc
Harshith <harshithl1777@gmail.com>
2024-12-03 21:18:58 -0500

    chore: reset db && add startup commands

--- startup.sh
+++ startup.sh
@@ -3,11 +3,14 @@
 echo "Starting application setup..."
 npm install
 
+echo "Starting application setup..."
+cd client && npm install
+
 echo "Building code execution Docker container..."
 docker build -t scriptorium .
 
 echo "Running Docker container..."
-docker run -dp 127.0.0.1:8080:8080 scriptorium
+docker run --name scriptorium -dp 127.0.0.1:8080:8080 scriptorium
 
 # Run database migrations
 npx prisma migrate deploy
